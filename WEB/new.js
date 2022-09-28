4fcfc8e92d2b9dd83193af5f30c337427e28f8ed
athillah <108055049+athillah@users.noreply.github.com>
2022-09-28 12:08:50 +0800

    Add files via upload

---
+++ WEB/new.js
@@ -0,0 +1,13 @@
+var a=document.getElementsByTagName("START").value
+
+function test(){
+    if (a == 1) {
+        document.write("test1");
+    }
+    else if (a == 0) {
+        document.write("test0");
+    }
+    else {
+        document.write("test3");
+    }
+}
